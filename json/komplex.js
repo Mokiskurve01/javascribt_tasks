06ba922967f152c4c8321e1d9674a3bdeea4c9e4
Mokiskurve01 <https://github.com/Mokiskurve01/java_kurs_1.git>
2022-01-30 18:48:20 +0100

    json augaben

--- json/komplex.js
+++ json/komplex.js
@@ -2,3 +2,15 @@
 
 // https://github.com/r-spacex/SpaceX-API
 // rockets.json: https://api.spacexdata.com/v3/rockets
+
+const fs = require("fs")
+const rocketAsString = fs.readFileSync("spacex/rockets.json", {
+  encoding: "utf-8"
+})
+
+const rockets = JSON.parse(rocketAsString)
+
+
+for (const rocket of rockets) {
+  console.log(rocket["rocket_name"])
+}
